d65cf246477611c7227ce67f0248c8676fa0edd1
Xueying Chen <41633812+xchen74@users.noreply.github.com>
2019-10-03 23:01:24 -0400

    upload files

---
+++ jialin.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 04</title>
+	<link rel="stylesheet" href="css/styles.css">
+  <body>
+
+<header>
+	<h1> Jialin Huo <h2>
+ </header>
+
+
+
+<nav>
+		<ul>
+			<li><a class="is-current" href="home.php">Home</a></li>
+			<li><a href="junting.php">Junting Chen</a></li>
+			<li><a href="richard.php">Richard O Akomea</a></li>
+			<li><a href="jialin.php">Jialin Huo</a></li>
+		</ul>
+
+</nav>
+
+<main>
+	
+		<p>Junting and Jialin are majoring in Business and minoring in CS at University of Rochester. Jinting took CSC 170 last semester for the foundation of CSC 174 this semester. She cares about exploring different opportunities in college to define her career path. Jialin cares about giving back to her community through volunteering and she has done an internship in investment banking. During her internship, she has participated in an acquisition deal and an Initial Public Offering in which the transaction values over thirty million dollars. 
+
+		<p>Richard is majoring in a different discipline. He is studying Computer Science as his major at University of Rochester. Richard comes from Ghana, whereas Junting and Jialin come from major cities in China. Junting and Jialin did not mention anything about their family in their biography, however Richard talks about his hometown and his family in depth.
+        </p>
+
+        <p>
+          Jinting and Jialin both have interest in arts. Since high school, Jialin has demonstrated strong interest in visual designs and various categories of art including calligraphy, painting, photography, and etc. Jinting likes painting activities at school. Richard has a hobby in soccer and has been playing soccer since age 7. 
+        </p>	
+	
+	</main>
+
+
+
+	<footer>CSC174: Advanced Front-end Web Design and Development</footer>
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="scripts/menu-highlighter.js"></script>
+
+</body>
+
+</html>
+
+</body>
+</html>
