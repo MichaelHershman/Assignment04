1714807f9db2ab6e40bb0880f1eea6801987e29b
Xueying Chen <41633812+xchen74@users.noreply.github.com>
2019-10-04 23:27:07 -0400

    upload

---
+++ jialin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 04</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+  <body>
+
+<header>
+	<a href= "index.php">
+	<h1> Jialin Huo </h1>
+	<img src= "images/jialin.jpg" alt="Jialin">	
+	</a>
+ </header>
+
+
+
+<nav>
+		<ul>
+			<li><a class="is-current" href="home.php">Home</a></li>
+			<li><a href="junting.php">Junting Chen</a></li>
+			<li><a href="richard.php">Richard O Akomea</a></li>
+			<li><a href="jialin.php">Jialin Huo</a></li>
+		</ul>
+
+</nav>
+
+<main>
+	
+		<p>
+		<h2> Business Major </h2>		
+		As a fifth-year student at the University of Rochester, I am double majoring in Business Finance and Art History, with a minor in Computer Science. Due to computer science and coding’s popularity and its extensive use in all kinds of fields and the business world, I have decided to re-classify from Class of 2019 to Class of 2020 in order to complete this challenging but extremely useful minor and coding abilities. Until now, I have acquired coding experience including Python, R Studio, HTML, CSS and little knowledge of JavaScript Plugins and I am thrilled to learn more from both my professors and classmates.
+		</p>
+
+		<p>
+		<h1> Hobby </h1>	
+		Outside of school works and internships, I spend my free time with my hobbies and moreover exploring my possibilities in different kinds of areas, including sports and music. I am particularly interested in playing tennis, swimming, skiing, and golf. During other times, I have dedicated my energy to philanthropy and charity activities in which I consider them significant and memorable. For six years, I have gone to Seattle annually and served as a volunteer to plan and hold fundraisers in order to help with the education of vision-impaired children; and the program has helped over fifty students to progress from elementary school through college.	
+        </p>
+
+        <p>
+        <h1> Work Experience </h1>	
+          While spending my summer break back in Shanghai in the past few years, I have had three internships in which two internships were at consulting companies and the other one was at an asset management company. Through the extensive training and the intensive work environment in consulting company, I have become proficient in data analysis through the use of coding and technical tools, reporting current market benchmarks and trends, and constructing interactive and animated electronic reports for customer use according to their desired criteria. Additionally, during the investment banking internship, I performed due diligence, prepared valuation analysis and client presentations. I have participated in an acquisition deal and an Initial Public Offering in which the transaction values over thirty million dollars.
+ 
+        </p>	
+	
+	</main>
+
+
+
+	<footer>CSC174: Advanced Front-end Web Design and Development</footer>
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="scripts/menu-highlighter.js"></script>
+
+</body>
+
+</html>
+
---
+++ richard.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 04</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+  <body>
+
+<header>
+	<a href= "index.php">
+	<h1> Business Major vs CS Major </h1>>
+	<img src= "images/richard.jpg" alt="Richard">	
+	</a>
+ </header>
+
+<nav>
+		<ul>
+			<li><a class="is-current" href="home.php">Home</a></li>
+			<li><a href="college.php">Major</a></li>
+			<li><a href="hobby.php">Hobby</a></li>
+			<li><a href="work.php">Work Experience</a></li>
+		</ul>
+
+</nav>
+
+<main>
+	
+		<p>
+		<h1> CS Major </h1>		
+		Richard Owusu Akomea was born on April 11, 1997, in Tarkwa, Ghana. He is majoring in Computer Science at University of Rochester. His father, Richard Kofi Akomea, is a Reverend Pastor and a practising Pentecostal from Amuanna Praso, Tano, and worked as a Farmer, working at the ministry of Forestry in his early career.Richard's mother, Joyce Arko Akomea, is an Akan from Akyim who worked as an English teacher and florist. His parents met after Richard held a sermon at Church of Pentecost- Koforidua, where he first interacted with Joyce, who was in attendance. He is a citizen of Ghana. In his youth, Richard attended a Catholic day school, and formally celebrated becoming a kindergarten graduate in a religious service.
+	</p>
+
+		<p>
+		<h1> Hobby <h1>		
+		As a child, Richard played for amateur team Young Kotoko from 2011 to 2012, in Ardoggon ,Spintex Road, and later spent a couple of months with Sweps FC . In 2014, aged 17, he went on a three-day trial with Sweps FC senior team, but didn't make the final cut for the 23 man team. He subsequently moved from Accra to Gbawe, where he joined the Gbawe DC school Local pick up team. By age 10, Richard believed he had the ability to play semi-professionally, and tried to convince his mother to cease his education in order to focus entirely on soccer. While popular with other students at school, he known rather for his academic achievements than his soccer playing skills or football as it is called in Ghana and everywhere else in the world. However, one year later, he was diagnosed with a right knee contusion, a condition that could have forced him to give up playing soccer.
+        </p>
+
+        <p>
+         Richard began his early production career in the mid-2010s, creating beats primarily for himself, an upcoming local artist, eventually developing a style that involved a combination of styles amany famous artists. His first official song came at the age of eighteen when he produced and rapped on YOLO, a single he put out in 2015 with Tony Dickson. For a while now, Richard has not released any musical projects but has appeared on other albums such as Jamal Damiens recital released on soundcloud. Richard also has a history of organising musical concerts with the 2015 Singspiration being evidence of that. In 2015 he alongside members of the Accra Academy Scripture Union Organised an Singspiration which saw a gathering of most of the prominent high schools in Accra at the time for a gospel music concert. 
+        </p>	
+	
+	</main>
+
+
+
+	<footer>CSC174: Advanced Front-end Web Design and Development</footer>
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="scripts/menu-highlighter.js"></script>
+
+</body>
+
+</html>
