644bbb4311592a7cabbfc06a3ce09fd07afbe800
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-10-08 22:00:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Assignment 04 | Business Major vs CS Major</title>
+	<title>Assignment 04</title>
 	<link rel="stylesheet" href="css/styles.css">
 	<script src="js/menu-highlighter.js"></script>
 </head>
