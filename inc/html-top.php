4101c1d54a376ef95055644636fea70c50c14674
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-10-08 22:19:20 -0400

    Add files via upload

---
+++ inc/html-top.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 04</title>
+	
+	<link rel="stylesheet" href="css/styles.css">
+	<script src="js/menu-highlighter.js"></script>
+	
+</head>
+
+
