697a62742c2f848d9f695f17885ea0181b14c7ac
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-10-12 00:05:58 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include "inc/html-top.php"; ?>
+<body class="view">
+<div class="container grid">
+	
+
+<header class="prim">
+ <!---Primary Optical Area--->
+	<a href= "index.php">
+	<h1> Business Major vs CS Major </h1>
+	
+</a>
+ </header>
+
+
+ <!---Strong Follow Area--->
+<nav class="str">
+		<ul>
+			<li><a class="is-current" href="index.php">Home</a></li>
+			<li><a href="students.php">Browse Students</a></li>
+		</ul>
+
+</nav>
+
+ <!---Weak Visual Area--->
+<main class="weak">	
+		<p>Jinting, Jialin and Richard are all taking CSC 174 this semester.
+		Junting and Jialin are majoring in Business at University of Rochester. 
+		Richard is special because he is studying Computer Science as his major at University of Rochester.
+        </p>
+
+	</main>
+
+
+
+ <!---Terminal Area--->
+	<footer class="term">
+	<section>
+	<h1> Attention! </h1>
+	learn more about those students
+	Two Business Majors vs One CS Major
+	</section>
+	<section>
+	CSC174: Advanced Front-end Web Design and Development
+	</section>
+	</footer>
+</div>
+	
+</body>
+
+</html>
+
---
+++ students.php
@@ -0,0 +1,70 @@
+<?php include "inc/html-top.php"; ?>
+<body>
+<div class="container">
+	
+
+<header class="flex">
+	<a href= "index.php">
+	<h1> Business Major vs CS Major </h1>
+	</a>
+ </header>
+
+
+
+<nav class="pos">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a class="is-current" href="students.php">Students</a></li>
+		</ul>
+
+</nav>
+<div class="grid2">
+	 <section>
+	 <a href= "#">
+	 <h1> Jialin Huo </h1>
+	 <img src= "images/jialin.jpg" alt="Jialin">	
+	 </a>
+		
+	 </section>	
+		<p>
+			
+		This is Jialin Huo. As a fifth-year student at the University of Rochester, I am double majoring in Business Finance and Art History, with a minor in Computer Science. Due to computer science and coding’s popularity and its extensive use in all kinds of fields and the business world, I have decided to re-classify from Class of 2019 to Class of 2020 in order to complete this challenging but extremely useful minor and coding abilities. Until now, I have acquired coding experience including Python, R Studio, HTML, CSS and little knowledge of JavaScript Plugins and I am thrilled to learn more from both my professors and classmates.
+		</p>
+
+	<section>
+	<a href= "#">
+	<h1> Junting Chen </h1>
+	<img src= "images/junting.jpg" alt="Junting">
+    </a>
+    	
+    </section>
+		<p>
+		
+		This is Junting Chen, a sophomore in University of Rochester, and I prefer Christy as my English name. I am major in Business and consider minoring in or BA computer science. Therefore, I took CSC 170 last semester for the foundation of CSC 174 this semester. Exploring opportunities during college life is my first goal, including life decisions, career path, and networking.
+	    </p>
+	
+
+		<main>
+	<a href= "#">
+	<h1> Richard Osasu Akomea </h1>
+	<img src= "images/richard.jpg" alt="Richard">	
+	</a>
+	
+     </main>
+		<p class="odd">
+		
+		Richard Owusu Akomea was born on April 11, 1997, in Tarkwa, Ghana. He is majoring in Computer Science at University of Rochester. His father, Richard Kofi Akomea, is a Reverend Pastor and a practising Pentecostal from Amuanna Praso, Tano, and worked as a Farmer, working at the ministry of Forestry in his early career.Richard's mother, Joyce Arko Akomea, is an Akan from Akyim who worked as an English teacher and florist. His parents met after Richard held a sermon at Church of Pentecost- Koforidua, where he first interacted with Joyce, who was in attendance. He is a citizen of Ghana. In his youth, Richard attended a Catholic day school, and formally celebrated becoming a kindergarten graduate in a religious service.
+		</p>
+		
+
+
+	<footer>CSC174: Advanced Front-end Web Design and Development</footer>
+	</div>
+</div>
+	
+	
+
+</body>
+
+</html>
+
